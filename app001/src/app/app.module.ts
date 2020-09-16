eda2df47c1737ec3a538e236b52766c0e87b3263
Rajesh Kannan <rrajeshkannan@gmail.com>
2020-09-16 18:13:24 +0530

    assignment for custom component

--- app001/src/app/app.module.ts
+++ app001/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { WarningComponent } from './notification_messages/warning/warning.component';
+import { AlertComponent } from './notification_messages/alert/alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    WarningComponent,
+    AlertComponent
   ],
   imports: [
     BrowserModule,
