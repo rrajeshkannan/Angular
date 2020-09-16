f6528eb79076ac170e0ce361e42b14a9312912e3
Rajesh Kannan <rrajeshkannan@gmail.com>
2020-09-16 17:38:45 +0530

    Inline component template through multiple lines

--- app001/src/app/servers/servers.component.ts
+++ app001/src/app/servers/servers.component.ts
@@ -3,7 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servers',
 /*templateUrl: './servers.component.html',*/
-  template: '<app-server></app-server><app-server></app-server>',
+  template: `
+    <app-server> </app-server>
+    <app-server> </app-server>`,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
