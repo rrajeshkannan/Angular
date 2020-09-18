eac11d5dc26613237ecf50a0f9eeac4f37384968
Rajesh Kannan <rrajeshkannan@gmail.com>
2020-09-18 18:20:06 +0530

    ngClass directive added

--- app003/src/app/server/server.component.ts
+++ app003/src/app/server/server.component.ts
@@ -3,13 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
-  styleUrls: ['./server.component.css'],
+  styles: [
+    `
+      .online {
+        color: white;
+      }
+    `,
+  ],
 })
 export class ServerComponent implements OnInit {
   serverId = 10;
   serverStatus = 'offline';
 
-  constructor() {}
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
 
   ngOnInit(): void {}
+
+  getColor(): string {
+    return this.serverStatus === 'online' ? 'lightgreen' : 'crimson';
+  }
 }
