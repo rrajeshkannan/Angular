7e0f50d2a933923e80f90427328b8bab048c9ecc
Rajesh Kannan <rrajeshkannan@gmail.com>
2020-09-18 19:29:45 +0530

    directive ngFor implemented!

--- app003/src/app/servers/servers.component.ts
+++ app003/src/app/servers/servers.component.ts
@@ -12,12 +12,14 @@ export class ServersComponent implements OnInit {
   serverName = '';
   serverCreationStatus = 'No server is created yet';
   serverCreated = false;
+  servers = ['TestServer-1', 'TestServer-2'];
 
   constructor() {}
 
   ngOnInit(): void {}
 
   onCreateServer(): void {
+    this.servers.push(this.serverName);
     this.serverCreationStatus = 'Server ' + this.serverName + ' is created';
     this.serverCreated = true;
   }
