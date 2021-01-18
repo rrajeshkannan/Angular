b64d5ae0bd41c2542cd02d25b261f96ba99c8b58
Rajesh Kannan <rrajeshkannan@gmail.com>
2021-01-18 12:47:36 +0530

    Adding a redirect

--- app007/angular-router-sample/src/app/app.module.ts
+++ app007/angular-router-sample/src/app/app.module.ts
@@ -5,14 +5,16 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
 import { HeroesListComponent } from './heroes-list/heroes-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
-  declarations: [AppComponent, CrisisListComponent, HeroesListComponent],
+  declarations: [AppComponent, CrisisListComponent, HeroesListComponent, PageNotFoundComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
       { path: 'crisis-list', component: CrisisListComponent },
       { path: 'heroes-list', component: HeroesListComponent },
+      { path: '', redirectTo: '/heroes-list', pathMatch: 'full' },
     ]),
   ],
   providers: [],
