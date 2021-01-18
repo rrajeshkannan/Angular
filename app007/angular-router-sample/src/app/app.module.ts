8cfe5942dc14829af9fc3ae2c1fe36b198d0fbad
Rajesh Kannan <rrajeshkannan@gmail.com>
2021-01-18 12:43:05 +0530

    Define routes and hyper-links to navigate

--- app007/angular-router-sample/src/app/app.module.ts
+++ app007/angular-router-sample/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { HeroesListComponent } from './heroes-list/heroes-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, CrisisListComponent, HeroesListComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'crisis-list', component: CrisisListComponent },
+      { path: 'heroes-list', component: HeroesListComponent },
+    ]),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
